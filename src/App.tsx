d0839057b0774ee27a61cbc46500c9b62382e352
Luke Callaghan <luke.callaghan23@gmail.com>
2025-11-29 23:45:28 -0500

    Quick swiping test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect, useRef } from 'react'
 import { createRoot } from 'react-dom/client'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
@@ -9,40 +9,158 @@ import PWABadge from './PWABadge.tsx'
 import './style/index.css'
 import './style/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {image} from './content/image.tsx';
+import {image2} from './content/image2.tsx';
+import {image3} from './content/image3.tsx';
+import {video} from './content/video.tsx';
+import {video2} from './content/video2.tsx';
+import {video3} from './content/video3.tsx';
+import { Displayable, type DisplayContent } from './components/Displayable.tsx'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="repic logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>repic</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <PWABadge />
-    </>
-  )
+export type Pos = [ number, number ];
+
+const initalMovementWindow: [Pos, Pos, Pos] = [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+];
+
+function App() {
+    const downPosition = useRef<Pos>(null);
+    const [ offset, setOffset ] = useState<Pos | null>(null);
+    const [ latestPosition, setLatestPosition ] = useState<Pos | null>(null);
+
+    const [ displayables, setDisplayables ] = useState<DisplayContent[]>([
+        [ 'image', image ],
+        [ 'image', image2 ],
+        [ 'image', image3 ],
+        [ 'video', video ],
+        [ 'video', video2 ],
+        [ 'video', video3 ],
+    ]);
+    const [ displayableIndex, setDisplayableIndex ] = useState<number>(0);
+    const moveDisplayableIndex = (direction: -1 | 1) => {
+        reset();
+        setDisplayableIndex(currentIdx => {
+            const tmp = (currentIdx + direction);
+            if (tmp < 0) {
+                return displayables.length - 1;
+            }
+            else if (tmp === displayables.length) {
+                return 0;
+            }
+            else return tmp;
+        })
+    };
+
+    const reset = () => {
+        downPosition.current = null;
+        setOffset(null);
+        setLatestPosition(null);
+        setMovementWindow(initalMovementWindow);
+    }
+
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [ movementWindow, setMovementWindow ] = useState<[Pos, Pos, Pos]>(initalMovementWindow);
+
+    return (
+        <>
+            <PWABadge />
+            <div 
+                id="video-container" 
+                className="video-container"
+                onMouseDown={ev => {
+                    downPosition.current = [
+                        ev.pageX,
+                        ev.pageY
+                    ];
+                }}
+                onMouseMove={ev => {
+                    if (downPosition.current) {
+                        const [ initialX, initialY ] = downPosition.current;
+                        
+                        setMovementWindow(prevWindow => {
+                            return [
+                                prevWindow[1],
+                                prevWindow[2],
+                                [ ev.movementX, ev.movementY ]
+                            ];
+                        });
+
+                        setOffset([
+                            (initialX - ev.pageX) * -1,
+                            (initialY - ev.pageY) * -1
+                        ]);
+                        setLatestPosition([
+                            ev.pageX,
+                            ev.pageY
+                        ]);
+                    }
+                }}
+                onMouseUp={() => {
+                    if (!containerRef.current) {
+                        return reset();
+                    }
+
+                    const [ accX, accY ]: Pos = movementWindow.reduce(([ aX, aY ], [ cX, cY ]) => {
+                        return [ aX + cX, aY + cY ];
+                    }, [ 0, 0 ]);
+
+                    const threshWidth = containerRef.current.clientWidth / 200;
+                    const threshHeight = containerRef.current.clientHeight / 10;
+
+                    if (Math.abs(accX) >= threshWidth) {
+                        if (accX < 0) {
+                            console.log("GO OFF LEFT");
+                            moveDisplayableIndex(-1);
+                        }
+                        else {
+                            console.log("GO OFF RIGHT");
+                            moveDisplayableIndex(1);
+                        }
+                    }
+                    else if (Math.abs(accY) >= threshHeight) {
+                        if (accY < 0) {
+                            console.log("GO OFF TOP");
+                        }
+                        else {
+                            console.log("GO OFF BOTTOM");
+                        }
+                    }
+
+                    reset();
+                }}
+                onMouseLeave={ev => {
+                    if (!offset) return;
+                    if (!containerRef.current) return;
+
+                    if (ev.clientX < 0.05) {
+                        console.log("WENT OFF LEFT");
+                        moveDisplayableIndex(-1);
+                    }
+                    else if (ev.clientY < 0.05) {
+                        console.log("WENT OFF TOP");
+                    }
+                    else if (ev.clientX / containerRef.current.clientWidth >= 0.95) {
+                        console.log("WENT OFF RIGHT");
+                        moveDisplayableIndex(1);
+                    }
+                    else if (ev.clientY / containerRef.current.clientHeight >= 0.95) {
+                        console.log("WENT OFF BOTTOM");
+                    }
+                }}
+                ref={containerRef}
+            >
+                <Displayable display={displayables[displayableIndex]} offset={offset} />
+            </div>
+        </>
+    )
 }
 
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <App />
+    </StrictMode>,
 );
 
---
+++ src/components/Displayable.tsx
@@ -0,0 +1,45 @@
+import type { Pos } from "../App";
+
+export type DisplayContent = [ 'video' | 'image', string ];
+
+export const Displayable = ({
+    display,
+    offset
+}: {
+    display: DisplayContent,
+    offset: Pos | null
+}) => {
+    const [ dType, dContent ] = display;
+
+    if (dType === 'image') {
+        return <img 
+            src={`data:image/png;base64,${dContent}`} 
+            style={
+                offset ? { 
+                    marginLeft: offset[0],
+                    marginTop: offset[1] 
+                } : {}
+            }
+            onDragStart={e => {e.preventDefault(); return false;}}
+        />
+    }
+    else {
+        return <video 
+            key={dContent}
+            autoPlay loop muted
+            style={
+                offset ? { 
+                    marginLeft: offset[0],
+                    marginTop: offset[1] 
+                } : {}
+            }
+            onDragStart={e => {e.preventDefault(); return false;}}
+        >
+            {(() => {
+                console.log(dContent.substring(0, 100))
+                return <></>
+            })()}
+            <source type="video/mp4" src={`data:video/mp4;base64,${dContent}`}/>
+        </video>
+    }
+}
